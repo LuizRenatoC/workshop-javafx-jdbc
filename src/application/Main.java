3288ddf885166d3641a3e6cfd18ee8b8bac73c1f
Luiz Renato <renatoperez107@gmail.com>
2023-03-08 15:09:29 -0300

    Saving a new Department

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,7 +9,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+	
 	private static Scene mainScene;
 	
 	@Override
@@ -29,11 +29,11 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static Scene getMainScene() {
 		return mainScene;
 	}
-
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
